6f82b33c69f7fd2825c4615e63abfb3ab12f816b
JiKay02 <supemantq@gmail.com>
2024-11-15 16:12:41 +0700

    yarn

--- config/firebase.js
+++ config/firebase.js
@@ -4,16 +4,18 @@ import { getFirestore } from "firebase/firestore";
 import Constants from "expo-constants";
 // Firebase config
 const firebaseConfig = {
-  apiKey: Constants.expoConfig.extra.apiKey,
-  authDomain: Constants.expoConfig.extra.authDomain,
-  projectId: Constants.expoConfig.extra.projectId,
-  storageBucket: Constants.expoConfig.extra.storageBucket,
-  messagingSenderId: Constants.expoConfig.extra.messagingSenderId,
-  appId: Constants.expoConfig.extra.appId,
-  databaseURL: Constants.expoConfig.extra.databaseURL,
+  apiKey: "AIzaSyBRRN1wP8ZdVsV2-O8BJRuEe9W8DD3r-N0",
+  authDomain: "chat-app-signal-59a5a.firebaseapp.com",
+  projectId: "chat-app-signal-59a5a",
+  storageBucket: "chat-app-signal-59a5a.firebasestorage.app",
+  messagingSenderId: "540808767557",
+  appId: "1:540808767557:web:bab841daa88227d7bedb2c"
   //   @deprecated is deprecated Constants.manifest
 };
 // initialize firebase
 initializeApp(firebaseConfig);
 export const auth = getAuth();
 export const database = getFirestore();
+
+
+// HuyPhung
